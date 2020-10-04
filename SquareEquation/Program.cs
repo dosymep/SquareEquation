2c0eda53cee5e45f1b9cb19223b2b1169ba5b805
dosymep <biseuv@yandex.ru>
2020-10-04 10:37:10 +0600

    Добавил консольный вывод сообщений

--- SquareEquation/Program.cs
+++ SquareEquation/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SquareEquation
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace SquareEquation {
+    public class Program {
+        public static void Main(string[] args) {
+            try {
+                ISolverFactory factory = SquareEquationFactory.CreateSolverFactory();
+
+                ISolver solver = factory.CreateSolver("2 5 -3.5");
+                Answer answer = solver.Solve();
+
+                Console.WriteLine(solver);
+                Console.WriteLine(answer);
+            } catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
