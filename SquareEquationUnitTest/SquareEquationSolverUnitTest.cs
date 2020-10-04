720d5742bbc5f1f4187830f73f62d0e15632f7af
dosymep <biseuv@yandex.ru>
2020-10-04 12:16:34 +0600

    Обновил список тестов

--- SquareEquationUnitTest/SquareEquationSolverUnitTest.cs
+++ SquareEquationUnitTest/SquareEquationSolverUnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
 using NUnit.Framework;
 using SquareEquation;
 
@@ -7,6 +8,19 @@ namespace SquareEquationUnitTest {
     [TestFixture]
     public class SquareEquationSolverUnitTest {
         private ISolverFactory _solverFactory;
+
+        public static IEnumerable FileTestCases {
+            get {
+                yield return Path.Combine(TestContext.CurrentContext.TestDirectory, "SolveTests.txt");
+            }
+        }
+
+        public static IEnumerable PositiveTestCases {
+            get {
+                yield return new TestCaseData("1 4 1").Returns(new Answer() { X1 = -0.2679, X2 = -3.7321 });
+                yield return new TestCaseData("2 5 -3.5").Returns(new Answer() { X1 = 0.5700, X2 = -3.0700 });
+            }
+        }
 
         [SetUp]
         public void SetUp() {
@@ -19,11 +33,36 @@ namespace SquareEquationUnitTest {
             return solver.Solve();
         }
 
-        public static IEnumerable PositiveTestCases {
-            get {
-                yield return new TestCaseData("2 5 -3.5").Returns(new Answer() { X1 = 0.5700, X2 = -3.0700 });
-                yield return new TestCaseData("1 4 1").Returns(new Answer() { X1 = -0.2679, X2 = -3.7321 });
-            }
+        [TestCase("1 0 1")]
+        [TestCase("1 1 1")]
+        public void SolveTestFailMethod(string @params) {
+            ISolver solver = _solverFactory.CreateSolver(@params);
+            Assert.Throws<Exception>(() => solver.Solve());
+        }
+
+        [TestCase("1")]
+        [TestCase("1 0 1 2")]
+        public void SolveFactoryTestArgumentExceptionMethod(string @params) {
+            Assert.Throws<ArgumentException>(() => _solverFactory.CreateSolver(@params));
+        }
+
+        [TestCase("1 0.9 1,2")]
+        [TestCase("DDD YYY SSS")]
+        public void SolveFactoryTestFormatExceptionMethod(string @params) {
+            // Не понял, почему double.Parce("1,2", CultureInfo.InvariantCulture) 
+            // не выбрасывает иселючение FormatException
+            Assert.Throws<FormatException>(() => _solverFactory.CreateSolver(@params));
+        }
+
+        [TestCase("SolveTestNotExist.txt")]
+        public void SolveTestFileFileNotFoundExceptionMethod(string filePath) {
+            Assert.Throws<FileNotFoundException>(() => _solverFactory.CreateSolverFromFile(filePath));
+        }
+
+        [TestCaseSource(nameof(FileTestCases))]
+        public void SolveTestFileMethod(string filePath) {
+            ISolver[] solvers = _solverFactory.CreateSolverFromFile(filePath);
+            Assert.AreEqual(solvers.Length, 4);
         }
     }
 }
