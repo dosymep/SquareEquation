c0dff17eaf81dd52ee3f1bbb1fe2cd664a6e913a
dosymep <biseuv@yandex.ru>
2020-10-04 11:41:22 +0600

    Добавил расширения сравнения

---
+++ SquareEquation/DoubleExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SquareEquation {
+    internal static class DoubleExtensions {
+        public static bool EqualsEps(this double source, double value, double epsilon = 0.001) {
+            return Math.Abs(source - value) < epsilon;
+        }
+    }
+}
+
