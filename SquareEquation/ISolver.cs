f95ebb7f533a71b93fcd94464bab4e3d7b052ec1
dosymep <biseuv@yandex.ru>
2020-10-04 10:17:28 +0600

    Добавил вспомогательные интерфейсы и фабрики

---
+++ SquareEquation/ISolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareEquation {
+    interface ISolver {
+        double A { get; }
+        double B { get; }
+        double C { get; }
+
+        Answer Solve();
+    }
+}
---
+++ SquareEquation/SquareEquationFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareEquation {
+    internal class SquareEquationFactory : ISolverFactory {
+        public ISolver CreateSolver(string @params) {
+            throw new NotImplementedException();
+        }
+    }
+}
