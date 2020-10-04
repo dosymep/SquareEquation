27ae045b200a67464ba4da42b354eee87c958b68
dosymep <biseuv@yandex.ru>
2020-10-04 10:20:14 +0600

    Добавил реализацию фабрики

--- SquareEquation/SquareEquationFactory.cs
+++ SquareEquation/SquareEquationFactory.cs
@@ -7,7 +7,16 @@ using System.Threading.Tasks;
 namespace SquareEquation {
     internal class SquareEquationFactory : ISolverFactory {
         public ISolver CreateSolver(string @params) {
-            throw new NotImplementedException();
+            string[] abc = @params.Split(' ');
+            if(abc.Length != 3) {
+                throw new ArgumentException("Введены неверные параметры уравнения.");
+            }
+
+            double a = double.Parse(abc[0]);
+            double b = double.Parse(abc[1]);
+            double c = double.Parse(abc[2]);
+
+            return new SquareEquationSolver() { A = a, B = b, C = c };
         }
     }
 }
