e53bce6431508b0795d293bab1965dabc2bd0e63
dosymep <biseuv@yandex.ru>
2020-10-04 10:14:26 +0600

    Написал решение уравнения

--- SquareEquation/SquareEquationSolver.cs
+++ SquareEquation/SquareEquationSolver.cs
@@ -1,7 +1,30 @@
-﻿namespace SquareEquation {
-    internal class SquareEquationSolver {
-        public void Solve(double a, double b, double c) {
+﻿using System;
 
+namespace SquareEquation {
+    internal class SquareEquationSolver {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Answer Solve() {
+            double discriminant = GetDiscriminant();
+            if(discriminant < 0) {
+                throw new Exception("Уравнение не имеет корней.");
+            }
+
+            return new Answer() { X1 = GetX1(discriminant), X2 = GetX2(discriminant) };
+        }
+
+        private double GetDiscriminant() {
+            return B * B - 4 * A * C;
+        }
+
+        private double GetX1(double discriminant) {
+            return (-B + Math.Sqrt(discriminant)) / (2 * A);
+        }
+
+        private double GetX2(double discriminant) {
+            return (-B - Math.Sqrt(discriminant)) / (2 * A);
         }
     }
 }
