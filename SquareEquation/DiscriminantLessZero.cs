f4fe637c5fd4880a55bc16e063f7bf9a98e1d6e3
dosymep <biseuv@yandex.ru>
2020-10-04 12:19:54 +0600

    Добавил исключение, если дискриминант равен нулю

---
+++ SquareEquation/DiscriminantLessZero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareEquation {
+    public class DiscriminantLessZeroException : Exception {
+        public DiscriminantLessZeroException() {
+        }
+
+        public DiscriminantLessZeroException(string message) : base(message) {
+        }
+
+        public DiscriminantLessZeroException(string message, Exception innerException) : base(message, innerException) {
+        }
+
+        protected DiscriminantLessZeroException(SerializationInfo info, StreamingContext context) : base(info, context) {
+        }
+    }
+}
