cd42f0934ddd6fabeb27bf6a4d66bbdfab096f35
dosymep <biseuv@yandex.ru>
2020-10-04 10:58:46 +0600

    Добавил проект тестов

---
+++ SquareEquationUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SquareEquationUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SquareEquationUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f36f419-ed01-455a-877c-710d71d3b2d1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SquareEquationUnitTest/SquareEquationSolverUnitTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SquareEquationUnitTest {
+    [TestClass]
+    public class SquareEquationSolverUnitTest {
+        [TestMethod]
+        public void SolveTestMethod() {
+        }
+    }
+}
