ef8e878d55ecd134a3fca7a6de1d18ad7f57ed18
dosymep <biseuv@yandex.ru>
2020-10-04 10:13:27 +0600

    Добавил класс ответа

---
+++ SquareEquation/Answer.cs
@@ -0,0 +1,10 @@
+﻿namespace SquareEquation {
+    internal class Answer {
+        public double X1 { get; set; }
+        public double X2 { get; set; }
+
+        public override string ToString() {
+            return base.ToString();
+        }
+    }
+}
