cdc40d1fdc5e9e209e8cb948a9aa4d0598448466
dosymep <biseuv@yandex.ru>
2020-10-04 11:42:26 +0600

    Добавил тесты для решателя уравнений

--- SquareEquationUnitTest/SquareEquationSolverUnitTest.cs
+++ SquareEquationUnitTest/SquareEquationSolverUnitTest.cs
@@ -1,11 +1,29 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using NUnit.Framework;
+using SquareEquation;
 
 namespace SquareEquationUnitTest {
-    [TestClass]
+    [TestFixture]
     public class SquareEquationSolverUnitTest {
-        [TestMethod]
-        public void SolveTestMethod() {
+        private ISolverFactory _solverFactory;
+
+        [SetUp]
+        public void SetUp() {
+            _solverFactory = SquareEquationFactory.CreateSolverFactory();
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public Answer SolveTestMethod(string @params) {
+            ISolver solver = _solverFactory.CreateSolver(@params);
+            return solver.Solve();
+        }
+
+        public static IEnumerable TestCases {
+            get {
+                yield return new TestCaseData("2 5 -3.5").Returns(new Answer() { X1 = 0.5700, X2 = -3.0700 });
+                yield return new TestCaseData("1 4 1").Returns(new Answer() { X1 = -0.2679, X2 = -3.7321 });
+            }
         }
     }
 }
